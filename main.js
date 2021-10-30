a010bc2cbffde057e4176e25fd1a6d0b30f3b0eb
gasmaskchan <soratavn@gmail.com>
2021-10-30 15:04:03 +0700

    z

--- main.js
+++ main.js
@@ -18,7 +18,7 @@ prefix = "~"
 client.discordTogether = new DiscordTogether(client)
 const pepenhi = ["hai anh", "haianh", "nhaianh", "hải anh", "bot"]
 const chonkk = ["chon", "chôn", "trôn", "troll"]
-const den = ["den lam", "đen lắm", "den vl", "đen vl", "đen vãi lồn", "đen"]
+const den = ["den lam", "đen lắm", "den vl", "đen vl", "đen vãi lồn", "đen", "nigga", "nigg", "nig", "nigger"]
 const black = ["đen", "den"]
 
 client.on("ready", () => {

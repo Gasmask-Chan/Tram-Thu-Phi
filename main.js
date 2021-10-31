12444f97aeeb5e354963caf0491edcf513ddf08f
gasmaskchan <soratavn@gmail.com>
2021-10-31 09:30:21 +0700

    c

--- main.js
+++ main.js
@@ -18,7 +18,7 @@ prefix = "~"
 client.discordTogether = new DiscordTogether(client)
 const pepenhi = ["hai anh", "haianh", "nhaianh", "hải anh", "bot"]
 const chonkk = ["chon", "chôn", "trôn", "troll"]
-const den = ["den lam", "đen lắm", "den vl", "đen vl", "đen vãi lồn", "đen thế", "den the", "nigga", "nigg", "nig", "nigger"]
+const den = ["den lam", "đen lắm", "den vl", "đen vl", "đen vãi lồn", "đen thế", "den the", "nigga", "nigg", "nig", "nigger","đen"]
 
 client.on("ready", () => {
   console.log("The bot is ready, LETS GOOOOOOOOOOO!")

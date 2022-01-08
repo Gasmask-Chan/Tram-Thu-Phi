c96dccb422b47a51c7de19eb846d44d1d2a4a5ca
gasmaskchan <soratavn@gmail.com>
2022-01-08 18:13:21 +0700

    dm thang ngu hai anh nhe

--- main.js
+++ main.js
@@ -573,6 +573,13 @@ client.on("message", msg => {
     })
   }
 
+  if (msg.content === `${prefix}haianh`) {
+    if (!msg.member.voice.channel) return msg.reply('You must to connect the voice channel to use this command!')
+    msg.member.voice.channel.join().then( connection =>{ 
+      connection.play('https://www.myinstants.com/media/sounds/ngu-cho-nay.mp3')
+    })
+  }
+
   if (msg.content === `${prefix}disconnect`) {
     if (!msg.member.voice.channel) return
     msg.member.voice.channel.leave()

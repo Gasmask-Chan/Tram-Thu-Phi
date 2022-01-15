37798e68a9d5cbcdb6db97bea23a8b278b264c31
gasmaskchan <soratavn@gmail.com>
2022-01-15 09:57:23 +0700

    dm nhaianh

--- main.js
+++ main.js
@@ -587,6 +587,13 @@ client.on("message", msg => {
     })
   }
 
+  if (msg.content === `${prefix}dmhaianh`) {
+    if (!msg.member.voice.channel) return msg.reply('You must to connect the voice channel to use this command!')
+    msg.member.voice.channel.join().then( connection =>{ 
+      connection.play('https://www.myinstants.com/media/sounds/dm-haianh.mp3')
+    })
+  }
+
   if (msg.content === `${prefix}disconnect`) {
     if (!msg.member.voice.channel) return
     msg.member.voice.channel.leave()

0700b422e19c37a480348ceb18bae8a7158c5267
gasmaskchan <soratavn@gmail.com>
2022-02-25 07:20:30 +0700

    change bot's activity

--- main.js
+++ main.js
@@ -26,7 +26,7 @@ client.on("ready", () => {
 
   client.user.setPresence({
     activity: {
-      name: "Super Idol的笑容都没你的甜八月正午的阳光都没你耀眼热爱 105 °C的你滴滴清纯的蒸馏水你不知道你有多可爱跌倒后会傻笑着再站起来"
+      name: "Escape From Bac Ky"
     }
   })
   
